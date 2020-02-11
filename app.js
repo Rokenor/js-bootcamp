c0badbe5a47ed592728e0881bfa858a5e9eada8c
Alexandr Kovrizhnykh <a.kovrizhnykh@gmail.com>
2020-02-11 19:34:04 +0200

    arrays

---
+++ app.js
@@ -0,0 +1,9 @@
+// Ex1
+
+let shoppingList = ['Cheddar cheese', '2% Milk'];
+
+shoppingList[1] = 'Whole Milk';
+shoppingList[2] = 'Ice Cream';
+shoppingList[shoppingList.length] = 'Tomatoes';
+shoppingList[shoppingList.length] = 'Potatoes';
+
