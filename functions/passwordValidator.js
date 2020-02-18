1378ade8870b895accbbd45b2e67b45d2c2715ff
Rokenor <a.kovrizhnykh@gmail.com>
2020-02-18 20:46:57 +0200

    function ex1

--- functions/passwordValidator.js
+++ functions/passwordValidator.js
@@ -12,11 +12,28 @@ function isValidPassword(password, username) {
   return true;
 }
 
-// short
+// shorter
 function isValidPasswordShort(password, username) {
   if (password.length < 8 || password.indexOf(' ') !== -1 || password.indexOf(username) !== -1) {
     return false;
   }
 
   return true;
+}
+
+// another way
+function isValidPasswordShort(password, username) {
+  const tooShort = password.length < 8;
+  const hasSpace = password.indexOf(' ') !== -1;
+  const tooSimilar = password.indexOf(username) !== -1;
+  if (tooShort || hasSpace || tooSimilar) return false;
+  return true;
+}
+
+// one more
+function isValidPasswordShort(password, username) {
+  const tooShort = password.length < 8;
+  const hasSpace = password.indexOf(' ') !== -1;
+  const tooSimilar = password.indexOf(username) !== -1;
+  return !tooShort && !hasSpace && !tooSimilar;
 }
