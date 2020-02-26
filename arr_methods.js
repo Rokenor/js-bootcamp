73e268a38b5d5da4676f6992b8390f2da63f8074
Rokenor <a.kovrizhnykh@gmail.com>
2020-02-26 18:12:53 +0200

    array methods

--- arr_methods.js
+++ arr_methods.js
@@ -1,6 +1,6 @@
-// --------
-// forEach
-// --------
+/**
+ * forEach
+ */
 const nums = [9,8,7,6,5,4,3,2,1];
 
 nums.forEach(function(n) {
@@ -62,4 +62,126 @@ const numDetail = numbers.map(function(n) {
 
 const abbrevs = words.map(function(word) {
   return word.toUpperCase().split('').join('.')
+});
+
+/**
+ * Find
+ */
+
+let movies = [
+   "The Fantastic Mr. Fox",
+   "Mr. and Mrs. Smith",
+   "Mrs. Doubtfire",
+   "Mr. Deeds"
+];
+
+let movie = movies.find(movie => {
+   return movie.includes('Mrs');
+})
+// "Mr. and Mrs. Smith"
+
+let movie2 = movies.find(m => m.indexOf('Mrs') === 0);
+// "Mrs. Doubtfire"
+
+
+const books = [
+  {
+    title: 'Good Omens',
+    authors: ['Terry Pratchett', 'Neil Gaiman'],
+    rating: 4.25
+  },{
+    title: 'Bone: The Complete Edition',
+    authors: ['Jeff Smith'],
+    rating: 4.42
+  },{
+    title: 'American Gods',
+    authors: ['Neil Gaiman'],
+    rating: 4.11
+  },{
+    title: 'A Gentelman in Moscow',
+    authors: ['Amor Towles'],
+    rating: 4.36
+  }
+];
+
+const goodBook = books.find(b => b.rating >= 4.3);
+const neilBook = books.find(b => b.includes('Neil Gaiman'));
+
+/**
+ * Filter
+ */
+
+const nums = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+const odds = nums.filter(n => {
+  return n % 2 === 1;
+})
+// [9, 7, 5, 3, 1]
+
+const smallNums = nums.filter(n => n < 5);
+// [4, 3, 2, 1]
+
+const books = [
+  {
+    title: 'Good Omens',
+    authors: ['Terry Pratchett', 'Neil Gaiman'],
+    rating: 4.25,
+    genres: ['fiction', 'fantasy']
+  },{
+    title: 'Changing My Mind',
+    authors: ['Zadie Smith'],
+    rating: 3.83,
+    genres: ['nonfiction', 'essays']
+  },{
+    title: 'Bone: The Complete Edition',
+    authors: ['Jeff Smith'],
+    rating: 4.42,
+    genres: ['fiction', 'graphic novel', 'fantasy']
+  },{
+    title: 'American Gods',
+    authors: ['Neil Gaiman'],
+    rating: 4.11,
+    genres: ['fiction', 'fantasy']
+  },{
+    title: 'A Gentelman in Moscow',
+    authors: ['Amor Towles'],
+    rating: 4.36,
+    genres: ['fiction', 'historical fiction']
+  },{
+    title: 'The Name of the Wind',
+    authors: ['Patrick Rothfuss'],
+    rating: 4.54,
+    genres: ['fiction', 'fantasy']
+  },{
+    title: 'The Overstory',
+    authors: ['Richard Powers'],
+    rating: 4.19,
+    genres: ['fiction', 'short stories']
+  },{
+    title: 'The Way of King',
+    authors: ['Brandon Sanderson'],
+    rating: 4.65,
+    genres: ['fantasy', 'epic']
+  },{
+    title: 'Lord of the files',
+    authors: ['William Golding'],
+    rating: 3.67,
+    genres: ['fiction']
+  }
+];
+
+const goodBooks = books.filter(b => b.rating > 4.3);
+
+const fantasyBooks = books.filter(book => (
+  book.genres.includes('fantasy')
+));
+
+const shortForm = books.filter(book => (
+  book.genres.includes('short stories') || book.genres.includes('essays')
+))
+
+const query = "";
+const results = books.filter(book => {
+  const title = book.title.toLowerCase();
+  return title.includes(query.toLowerCase())
 });
