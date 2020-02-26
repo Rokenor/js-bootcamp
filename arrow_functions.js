a5efe0b00509bb27bcffd6b186c7a3b799cfd8f1
Rokenor <a.kovrizhnykh@gmail.com>
2020-02-26 17:28:26 +0200

    arrow functions

---
+++ arrow_functions.js
@@ -0,0 +1,42 @@
+const sum = (x, y) => {
+  return x + y;
+}
+
+// === === === === ===
+
+const square = function(x) {
+  return x * x;
+}
+
+const square = (x) => {
+  return x * x;
+}
+
+// === === === === ===
+
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+
+const isEven = (num) => {
+  return num % 2 === 0;
+}
+
+const isEven = num => {
+  return num % 2 === 0;
+}
+
+const isEven = num => (
+  num % 2 === 0
+)
+
+const isEven = num => num % 2 === 0;
+
+// === === === === ===
+
+const multiply = (x, y) => {
+  return x * y;
+}
+
+// === === === === ===
+
