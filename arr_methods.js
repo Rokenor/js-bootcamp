d75672b15dd8186e149748dba9dd3d292694a0c9
Alexandr Kovrizhnykh <a.kovrizhnykh@gmail.com>
2020-02-28 23:07:01 +0200

    some & every

--- arr_methods.js
+++ arr_methods.js
@@ -185,3 +185,34 @@ const results = books.filter(book => {
   const title = book.title.toLowerCase();
   return title.includes(query.toLowerCase())
 });
+
+/**
+ * Every
+ */
+
+ const words = ['dog', 'dig', 'log', 'bag', 'wag'];
+
+ words.every(word => {
+   return word.length === 3;
+ }) // true
+
+ words.every(word => word[0] === 'd'); // false
+
+ words.every(w => {
+   let last_letter = w[w.length - 1];
+   return last_letter === 'g';
+ })
+
+ /**
+ * Some
+ */
+
+ const words = ['dog', 'jello', 'log', 'cupcake', 'bag', 'wag'];
+
+ words.some(word => {
+   return word.length > 4;
+ }); // true
+
+ words.some(word => word[0] === 'Z'); // false
+
+ words.some(w => w.includes('cake')); // true
