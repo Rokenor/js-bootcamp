4a5fee165ffa4803bf2399c0f865ebddffe603eb
Alexandr Kovrizhnykh <a.kovrizhnykh@gmail.com>
2020-03-02 23:47:58 +0200

    reduce

--- reduce.js
+++ reduce.js
@@ -11,3 +11,23 @@ const product = nums.reduce((total, currenVal) => {
 
 // ---------------
 
+const grades = [87, 64, 96, 92, 88, 99, 73, 70, 64];
+
+const maxGrade = grades.reduce((max, currVal) => {
+  if (currVal > max) return currVal;
+  return max;
+});
+
+const maxGrade = grades.reduce((max, currVal) => {
+  return Math.max(max, curreVal)
+});
+
+const minGrade = grades.reduce((min, currVal) => {
+  return Math.min(min, currVal)
+});
+
+// ---------------
+
+[10,20,30,40,50].reduce((sum, currVal) => {
+  return sum + currVal;
+}, 1000);
