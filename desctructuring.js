11ee8acfcad16d16cd59708bf996e488ce03b766
Alexandr Kovrizhnykh <a.kovrizhnykh@gmail.com>
2020-03-10 00:05:16 +0200

    desctructuring

--- desctructuring.js
+++ desctructuring.js
@@ -17,8 +17,57 @@ const runner = {
   country: 'Kenya',
   title: 'Elder of the Order of the Golden Heart of Kenya'
 }
-const {first, last, country} = runner;
+const {first, last, country, title: honorific} = runner;
 
-first: // "Eliud"
-last: // "Kipchoge"
+first; // "Eliud"
+last; // "Kipchoge"
 country; // "Kenya"
+honorific; // 'Elder of the Order of the Golden Heart of Kenya'
+
+const { first, last, ...other } = runner;
+
+first; // "Eliud"
+last; // "Kipchoge"
+other; // {country: 'Kenya', title: 'Elder of the Order of the Golden Heart of Kenya'}
+
+// ---------------
+
+const results = [
+  {
+    first: 'Eliud',
+    last: 'Kipchoge',
+    country: 'Kenya'
+  },
+  {
+    first: 'Feyisa',
+    last: 'Lilesa',
+    country: 'Ethiopia'
+  },
+  {
+    first: 'Galen',
+    last: 'Rupp',
+    country: 'United States'
+  }
+];
+
+const [{first: goldWinner}, { country }] = results; // goldWinner: Eliud; country: Ethiopia
+
+// ---------------
+
+const fullName = ({first, last}) => {
+  return `${first} ${last}`
+}
+const runner = {
+  first: 'Eliud',
+  last: 'Kipchoge',
+  country: 'Kenya',
+}
+fullName(runner); // 'Eliud Kipchoge'
+
+// ---------------
+
+function parseResponse([protocol, statusCode, contentType]) {
+  console.log(`Status: ${statusCode}`)
+}
+
+
