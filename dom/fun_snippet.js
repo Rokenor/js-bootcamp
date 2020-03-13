d0d8901c51888f386a1b7a924025603f5074134a
Alexandr Kovrizhnykh <a.kovrizhnykh@gmail.com>
2020-03-13 19:03:57 +0200

    dom snippet

--- dom/fun_snippet.js
+++ dom/fun_snippet.js
@@ -1,12 +1,21 @@
 const myImg = document.createElement('img');
 myImg.src = '' // some random image
+
 myImg.style.width = '200px';
 document.body.append(myImg);
 myImg.style.transition = 'all 2s';
 
-setInterval(() => {
-  const x = Math.floor(document.body.clientWidth * Math.random());
-  const y = Math.floor(document.body.clientHeight * Math.random());
+const sheet = new CSSStyleSheet();
+sheet.replaceSync('* {transition: all 2s}');
+document.adoptedStyleSheets = [sheet];
 
-  myImg.style.transform = `translate(${x}px,${y}px)`;
+const allEls = document.body.children;
+
+setInterval(() => {
+  for(let el of allEls) {
+    const rotation = Math.floor(Math.randowm() * 360);
+    const x = Math.floor(document.body.clientWidth * Math.random());
+    const y = Math.floor(document.body.clientHeight * Math.random());
+    el.style.transform = `translate(${x}px,${y}px) rotate(${rotation}deg)`;
+  }
 }, 2000)
