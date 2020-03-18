45462016e25debe9a61c7bf57c89c99389d3b6f2
Alexandr Kovrizhnykh <a.kovrizhnykh@gmail.com>
2020-03-18 19:53:02 +0200

    DOM manipulate

---
+++ manipulate.js
@@ -0,0 +1,16 @@
+// important properties and methods from DOM
+
+const h1 = document.querySelector('h1');
+
+h1.innerText // only element text without html elements
+document.body.innerText // all text at webpage
+
+h1.innerText = "Some text" // h1 text will be updated
+
+h1.textContent // same h1.innerText but formatted and with scripts
+
+
+
+
+
+
